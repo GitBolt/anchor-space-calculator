315dece0b382858f1a26ab170b8bc27df0590267
GitBolt <saabisakhtar@gmail.com>
2023-06-06 16:21:14 +0530

    add: og images

--- src/components/DefaultHead.tsx
+++ src/components/DefaultHead.tsx
@@ -5,8 +5,19 @@ export const DefaultHead = () => {
     <Head>
       <title>Anchor Space Calculator</title>
       <meta name="description" content="Calculate your Anchor Program Data Account space" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <meta name="image" content="https://anchorspace.vercel.app/og.png" />
+      <meta property="og:title" content={"Anchor Space Calculator"} />
+      <meta property="og:description" content={"Calculate your Anchor Program Data Account space."} />
+      <meta property="og:image" content="https://anchorspace.vercel.app/og.png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://anchorspace.vercel.app/og.png" />
+      <meta property="og:site_name" content={"Anchor Space Calculator"} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={"Anchor Space Calculator"} />
+      <meta name="twitter:description" content={"Calculate your Anchor Program Data Account space"} />
+      <meta name="twitter:image" content="https://anchorspace.vercel.app/og.png" />
+      <meta name="twitter:image:alt" content={"Calculate your Anchor Program Data Account space"} />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
   )
 }
