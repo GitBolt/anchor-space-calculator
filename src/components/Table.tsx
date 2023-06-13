c9c3a47ae5296906f188670122d09106a0f987ca
GitBolt <saabisakhtar@gmail.com>
2023-06-13 18:01:19 +0530

    add: code copy paste

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -13,10 +13,12 @@ import {
   NumberInputField,
   NumberInput,
   Box,
-  useClipboard,
-  useToast
+  useToast,
+  Button
 } from "@chakra-ui/react";
 import { useState } from "react";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { androidstudio, dracula, monokai } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 type SpaceData = {
   type: string;
@@ -26,7 +28,8 @@ type SpaceData = {
 
 const TableComponent: React.FC<{ spaceData: Record<string, SpaceData> }> = ({ spaceData }) => {
   const [additionalSpace, setAdditionalSpace] = useState<Record<string, number>>({});
-  const [parsedSpace, setParsedSpace] = useState<any>()
+  const [showCode, setShowCode] = useState<boolean>(false)
+
   const handleAdditionalSpaceChange = (field: number, value: string) => {
     setAdditionalSpace((prevState) => ({
       ...prevState,
@@ -103,101 +106,140 @@ const TableComponent: React.FC<{ spaceData: Record<string, SpaceData> }> = ({ sp
     return totalBytes;
   };
 
+  const generateRustCodeWithComments = () => {
+    let rustCode = 'impl MyData {\n    pub const SIZE: usize =\n';
+    Object.entries(spaceData).forEach(([fieldName, { space, prefix_space, type }]) => {
+      const additionalSpaceValue = additionalSpace[space] || 0;
+
+      if (prefix_space) {
+        if (type === "String") {
+          rustCode += `    + (${prefix_space} + ${space || additionalSpaceValue}) // ${fieldName}\n`;
+        } else {
+          rustCode += `    + (${prefix_space} + ${space}`;
+          if (type.startsWith("Vec<") && additionalSpaceValue) {
+            rustCode += ` * ${additionalSpaceValue}) // ${fieldName}\n`;
+          } else {
+            rustCode += ") // " + fieldName + "\n";
+          }
+        }
+      } else {
+        rustCode += `    + ${space} // ${fieldName}\n`;
+      }
+    });
+
+    return rustCode;
+
+  };
+
 
   return (
     <>
       <Flex bg="#0E0E17" w="100%" pos="relative" maxH="70vh" flexFlow="column" p="10px 20px" rounded="10px">
-        <Text fontSize="20px" color="#60569E">Space Required</Text>
-
-        <Flex justify="space-between" align="center">
-          <Text fontSize="25px" color="white" fontWeight={800}>{calculateTotalSpace()}</Text>
-          <CopyIcon
-            onClick={() => {
-              navigator.clipboard.writeText(calculateTotalSpace())
-              toast({
-                status: "success",
-                title: "Successfully copied space"
-              })
-            }}
-            color="white" w="2rem" h="2rem" sx={{ cursor: "pointer" }}
-          />
+        <Flex w="100%" justify="space-between" mb="1rem">
+          <Button h="2.2rem" w="50%" colorScheme="facebook" _hover={{ bg: "none" }} onClick={() => setShowCode(false)} variant={showCode ? "outline" : "solid"}>Space Breakdown</Button>
+          <Button h="2.2rem" w="50%" colorScheme="facebook" _hover={{ bg: "none" }} onClick={() => setShowCode(true)} variant={!showCode ? "outline" : "solid"}>Rust Code</Button>
         </Flex>
 
+        {showCode ? <>
+          <SyntaxHighlighter language="rust" style={androidstudio} customStyle={{fontSize:"1.3rem", background:'#0E0E17'}}>
+            {generateRustCodeWithComments()}
+          </SyntaxHighlighter>
 
-        <Divider borderColor="gray.800" my='10px' />
-        <Text fontSize="20px" color="#60569E">Total Byte Space</Text>
-        <Text fontSize="25px" color="white" fontWeight={700}>{calculateTotalBytes()} Bytes</Text>
+        </> : <>
 
-        <Divider borderColor="gray.800" my='10px' />
-        <Text fontSize="20px" color="#60569E" mb="10px">Breakdown</Text>
+          <Text fontSize="20px" color="#60569E">Space Required</Text>
+
+          <Flex justify="space-between" align="center">
+            <Text fontSize="25px" color="white" fontWeight={800}>{calculateTotalSpace()}</Text>
+            <CopyIcon
+              onClick={() => {
+                navigator.clipboard.writeText(calculateTotalSpace())
+                toast({
+                  status: "success",
+                  title: "Successfully copied space"
+                })
+              }}
+              color="white" w="2rem" h="2rem" sx={{ cursor: "pointer" }}
+            />
+          </Flex>
 
 
-        <Box overflowX="auto" flex="1">
-          <Table bg="#161723" variant="simple" colorScheme="purple" rounded="10px">
-            <Thead pos="static">
-              <Tr pos="static">
-                <Th pos="static" color="#599CFF" fontSize="15px">Field</Th>
-                <Th color="#599CFF" fontSize="15px">Type</Th>
-                <Th color="#599CFF" fontSize="15px" minWidth="120px">Space</Th>
-                <Th color="#599CFF" fontSize="15px">Item Size / Note</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr opacity="0.5">
-                <Td color="#8FA1FF" fontStyle="italic" fontWeight={600}>Discriminator</Td>
-                <Td color="#B84173" fontStyle="italic" fontWeight={600}>u8</Td>
-                <Td color="#8FA1FF" fontStyle="italic">
-                  8
-                </Td>
-                <Td color="gray.600" fontWeight={600} fontStyle="italic">
-                  Required by Default
-                </Td>
-              </Tr>
-              {Object.entries(spaceData).map(([field, { type, space, prefix_space }]) => (
-                <Tr key={field} bg={(type.startsWith("Vec<") || type == "String") ? "#1d1f37" : "transparent"}>
-                  <Td color="#8FA1FF" fontWeight={600}>{field}</Td>
-                  <Td color="#B84173" fontWeight={600}>{type}</Td>
-                  <Td color="#8FA1FF" minWidth="120px">
-                    {renderSpaceValue({ type, space, prefix_space })}
-                  </Td>
-                  {(type.startsWith("Vec<") || type == "String") ? <Td color="#8FA1FF">
-                    <NumberInput>
+          <Divider borderColor="gray.800" my='10px' />
+          <Text fontSize="20px" color="#60569E">Total Byte Space</Text>
+          <Text fontSize="25px" color="white" fontWeight={700}>{calculateTotalBytes()} Bytes</Text>
 
-                      <NumberInputField
-                        border="1px solid"
-                        borderColor="blue.500"
-                        type="number"
-                        padding="5px"
-                        min="1"
-                        placeholder="Enter amount"
-                        onChange={(e) => handleAdditionalSpaceChange(space, e.target.value)}
-                        borderBottomWidth="1px"
-                        _hover={{ borderBottomWidth: "1px" }}
-                      />
-                    </NumberInput>
 
-                  </Td> :
-                    Object.keys(spaceReference).includes(type) ?
-                      <Td color="gray.600" fontWeight={600} fontStyle="italic">
-                        Not dynamic value
-                      </Td> :
+          <Divider borderColor="gray.800" my='10px' />
+          <Text fontSize="20px" color="#60569E" mb="10px">Breakdown</Text>
 
-                      type.match(/^\[.*;.*\]$/) ?
-                        <Td color="gray.500" fontWeight={600} fontStyle="italic">
-                          {type.split(";")[0].slice(1)} ({spaceReference[type.split(";")[0].slice(1)]}) * {type.split(";")[1].replace("]", "")}
-                        </Td>
-                        :
-                        <Td color="gray.500" fontWeight={600} fontStyle="italic">
-                          Largest value from enum is picked
-                        </Td>
-                  }
 
+          <Box overflowX="auto" flex="1">
+            <Table bg="#161723" variant="simple" colorScheme="purple" rounded="10px">
+              <Thead pos="static">
+                <Tr pos="static">
+                  <Th pos="static" color="#599CFF" fontSize="15px">Field</Th>
+                  <Th color="#599CFF" fontSize="15px">Type</Th>
+                  <Th color="#599CFF" fontSize="15px" minWidth="120px">Space</Th>
+                  <Th color="#599CFF" fontSize="15px">Item Size / Note</Th>
                 </Tr>
-              ))}
-            </Tbody>
+              </Thead>
+              <Tbody>
+                <Tr opacity="0.5">
+                  <Td color="#8FA1FF" fontStyle="italic" fontWeight={600}>Discriminator</Td>
+                  <Td color="#B84173" fontStyle="italic" fontWeight={600}>u8</Td>
+                  <Td color="#8FA1FF" fontStyle="italic">
+                    8
+                  </Td>
+                  <Td color="gray.600" fontWeight={600} fontStyle="italic">
+                    Required by Default
+                  </Td>
+                </Tr>
+                {Object.entries(spaceData).map(([field, { type, space, prefix_space }]) => (
+                  <Tr key={field} bg={(type.startsWith("Vec<") || type == "String") ? "#1d1f37" : "transparent"}>
+                    <Td color="#8FA1FF" fontWeight={600}>{field}</Td>
+                    <Td color="#B84173" fontWeight={600}>{type}</Td>
+                    <Td color="#8FA1FF" minWidth="120px">
+                      {renderSpaceValue({ type, space, prefix_space })}
+                    </Td>
+                    {(type.startsWith("Vec<") || type == "String") ? <Td color="#8FA1FF">
+                      <NumberInput>
 
-          </Table>
-        </Box>
+                        <NumberInputField
+                          border="1px solid"
+                          borderColor="blue.500"
+                          type="number"
+                          padding="5px"
+                          min="1"
+                          placeholder="Enter amount"
+                          onChange={(e) => handleAdditionalSpaceChange(space, e.target.value)}
+                          borderBottomWidth="1px"
+                          _hover={{ borderBottomWidth: "1px" }}
+                        />
+                      </NumberInput>
+
+                    </Td> :
+                      Object.keys(spaceReference).includes(type) ?
+                        <Td color="gray.600" fontWeight={600} fontStyle="italic">
+                          Not dynamic value
+                        </Td> :
+
+                        type.match(/^\[.*;.*\]$/) ?
+                          <Td color="gray.500" fontWeight={600} fontStyle="italic">
+                            {type.split(";")[0].slice(1)} ({spaceReference[type.split(";")[0].slice(1)]}) * {type.split(";")[1].replace("]", "")}
+                          </Td>
+                          :
+                          <Td color="gray.500" fontWeight={600} fontStyle="italic">
+                            Largest value from enum is picked
+                          </Td>
+                    }
+
+                  </Tr>
+                ))}
+              </Tbody>
+
+            </Table>
+          </Box></>}
+
       </Flex>
 
     </>
